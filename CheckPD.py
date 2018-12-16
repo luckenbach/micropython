ac32e915887df4a9e4ed56a1de66d7883baca02b
Zach Hilliard <luckenbach@gmail.com>
2018-12-16 00:57:57 -0600

    updates to code base

--- CheckPD.py
+++ CheckPD.py
@@ -25,7 +25,7 @@ def do_connect():
     if not sta_if.isconnected():
         print('connecting to network...')
         sta_if.active(True)
-        sta_if.connect('FBI Van 24', 'hammertime')
+        sta_if.connect('', '')
         while not sta_if.isconnected():
             pass
     print('network config:', sta_if.ifconfig())
